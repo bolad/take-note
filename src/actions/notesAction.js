daf4f2e42538b2a4a3c636fa94a2ced53a57d633
stanley akyea <stanley.akyea@gmail.com>
2020-03-11 21:56:26 +0100

    Dispatch User and Notes status during and after loading

--- src/actions/notesAction.js
+++ src/actions/notesAction.js
@@ -1,17 +1,34 @@
-import { GET_NOTES } from '../actionTypes';
+import { GET_NOTES, NOTES_STATUS } from '../actionTypes';
 import { database } from '../firebase';
 
 export function getNotes() {
   //dispatch the action to the reducer
   return dispatch => {
+    //Set loading to true before notes are loaded
+    dispatch({
+      type: NOTES_STATUS,
+      payload: true
+    });
+
     //get data from firebase
     database.on('value', snapshot => {
-      //dispatch the type and payload to thne reducer
+      //dispatch the type and payload to the reducer
       dispatch({
         type: GET_NOTES,
         payload: snapshot.val()
-      })
-    })
+      });
+      //Set loading to false as after loading notes
+      dispatch({
+        type: NOTES_STATUS,
+        payload: false
+      });
+    }, {
+      //Wait until something changes and try again
+      dispatch({
+        type: NOTES_STATUS,
+        payload: -1
+      });
+    });
   }
 }
 
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,13 +1,23 @@
 import { auth, googleProvider, twitterProvider } from '../firebase';
-import { GET_USER } from '../actionTypes';
+import { GET_USER, USER_STATUS } from '../actionTypes';
 
 export function getUser() {
   return dispatch => {
+    //Show loading status as true before getting user
+    dispatch({
+      type: USER_STATUS,
+      payload: true
+    });
     auth.onAuthStateChanged(user => {
       dispatch({
         type: GET_USER,
         payload: user
       });
+      //Show loading status as false after getting user
+      dispatch({
+        type: USER_STATUS,
+        payload: false
+      })
     });
   }
 
